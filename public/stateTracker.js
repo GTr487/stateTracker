430bd849830044d117ac61e6ba98dd327a1a4049
GTR487 <gunther.molina.487@gmail.com>
2019-07-15 12:19:50 -0300

    refactorizacion

--- public/stateTracker.js
+++ public/stateTracker.js
@@ -1,11 +1,10 @@
 (function($){
     $.fn.StateTracker = function(options){
-        const regExp = /^#([0-9a-f]{3}){1,2}$/i;
-
         var defaults = {
             stages: [
                 {
-                    name: "stage 1",
+                    title: "stage 1",
+                    subtitle: "Bruce Wayne",
                     state: "Pending",
                     index: "0"
                 }
@@ -18,98 +17,110 @@
                 },
             ],
             colors: {
-                title: '#000000',
                 icon: '#000000',
+                title: '#000000',
+                subtitle: '#666666',
                 arrow: '#000000',
                 bgIcon: 'transparent',
                 bdIcon: 'transparent'
+            },
+            sizes: {
+                icon: 65,
+                title: 14,
+                subtitle: 14
             }
         }
 
-        var settings = $.extend({}, defaults, options);
-
+        var settings = defaults;
+        if(options && options.stages) $.extend(settings.stages, options.stages);
+        if(options && options.states) $.extend(settings.states, options.states);
+        if(options && options.colors) $.extend(settings.colors, options.colors);
+        if(options && options.sizes)  $.extend(settings.sizes,  options.sizes);
 
         /**
          * Create default CSS Class in the html DOM
-         * @param {String} [titleColor='#000000'] Color of the title
-         * @param {String} [IconColor='#000000']  Color of the Icon
-         * @param {String} [arrowColor='#000000'] Color of the arrows
-         * @param {String} [backIconColor='#FFFFFF'] Color of the Icon Background
-         * @param {String} [borderIconColor='#EEEEEE'] Color of the border of the icon container
          */
-        var setDefaultClasses = function(titleColor, IconColor, arrowColor, backIconColor, borderIconColor){
-            if(!regExp.test(titleColor))      titleColor      = '#000000';
-            if(!regExp.test(IconColor))       IconColor       = '#000000';
-            if(!regExp.test(arrowColor))      arrowColor      = '#000000';
-            if(!regExp.test(backIconColor))   backIconColor   = 'transparent';
-            if(!regExp.test(borderIconColor)) borderIconColor = 'transparent';
+        var setDefaultClasses = function(){
             
-            borderIconColor
             $('<style></style>')
                 .prop('type', 'text/css')
                 .html('\
-                .stTracker{                                  \
-                    font-size: 16px;                         \
-                }                                            \
-                .stTracker__cont{                            \
-                    padding: .5em;                           \
-                    display: flex;                           \
-                    flex-wrap: wrap;                         \
-                }                                            \
-                .stTracker__box{                             \
-                    display: flex;                           \
-                    margin-top: 1em;                         \
-                }                                            \
-                .stTracker__stage{                           \
-                    text-align: center;                      \
-                }                                            \
-                .stTracker__icon{                            \
-                    color: ' +IconColor+ ';                  \
-                    font-size: 2em;                          \
-                    background: ' +backIconColor+ ';         \
-                    border: 1px solid ' +borderIconColor+ '; \
-                    display: flex;                           \
-                    justify-content: center;                 \
-                    align-items: center;                     \
-                    padding: .5em;                           \
-                    border-radius: 50%;                      \
-                }                                            \
-                .stTracker__icon i{                          \
-                    width:1em;                               \
-                    height: 1em;                             \
-                }                                            \
-                .stTracker__title{                           \
-                    margin-top: .5em;                        \
-                    color: ' +titleColor+ ';                 \
-                }                                            \
-                .stTracker__arrow{                           \
-                    display: flex;                           \
-                    align-items: center;                     \
-                    padding-bottom: 1.5em;                   \
-                    margin: 0 2em 0 1.5em;                   \
-                }                                            \
-                .stTracker__arrow span{                      \
-                    display: inline-block;                   \
-                    padding: .5em;                           \
-                    border: solid ' +arrowColor+ ';          \
-                    border-width: 0 3px 3px 0;               \
-                }                                            \
-                .arrow--U span{                              \
-                    transform: rotate(-135deg);              \
-                }                                            \
-                .arrow--R span{                              \
-                    transform: rotate(-45deg);               \
-                }                                            \
-                .arrow--D span{                              \
-                    transform: rotate(45deg);                \
-                }                                            \
-                .arrow--L span{                              \
-                    transform: rotate(135deg);               \
-                }                                            \
+                .stTracker{                                         \
+                    font-size: 16px;                                \
+                }                                                   \
+                .stTracker__cont{                                   \
+                    padding: .5em;                                  \
+                    display: flex;                                  \
+                    flex-wrap: wrap;                                \
+                }                                                   \
+                .stTracker__box{                                    \
+                    display: flex;                                  \
+                    margin-top: 1em;                                \
+                }                                                   \
+                .stTracker__stage{                                  \
+                    text-align: center;                             \
+                }                                                   \
+                .stTracker__icon{                                   \
+                    width: ' +settings.sizes.icon+ 'px;             \
+                    height: ' +settings.sizes.icon+ 'px;            \
+                    color: ' +settings.colors.icon+ ';              \
+                    font-size: 2em;                                 \
+                    background: ' +settings.colors.bgIcon+ ';       \
+                    border: 1px solid ' +settings.colors.bdIcon+ '; \
+                    display: flex;                                  \
+                    justify-content: center;                        \
+                    align-items: center;                            \
+                    border-radius: 50%;                             \
+                    margin: auto                                    \
+                }                                                   \
+                .stTracker__icon i{                                 \
+                    width:1em;                                      \
+                    height: 1em;                                    \
+                }                                                   \
+                .stTracker__title{                                  \
+                    color: ' +settings.colors.title+ ';             \
+                    font-weight: bold;                              \
+                    font-size: ' +settings.sizes.title+ 'px;        \
+                    margin-bottom: -0.25em;                         \
+                }                                                   \
+                .stTracker__subtitle {                              \
+                    color: ' +settings.colors.subtitle+ ';          \
+                    font-size: ' +settings.sizes.title+ 'px;        \
+                    display: inline-block;                          \
+                    line-height: 1em;                               \
+                }                                                   \
+                .stTracker__subtitle p,                             \
+                .stTracker__title p {                               \
+                    margin: 0;                                      \
+                }                                                   \
+                .stTracker__arrow{                                  \
+                    display: flex;                                  \
+                    align-items: center;                            \
+                    padding-bottom: 1.5em;                          \
+                    margin: 0 2em 0 1.5em;                          \
+                }                                                   \
+                .stTracker__arrow span{                             \
+                    display: inline-block;                          \
+                    padding: .5em;                                  \
+                    border: solid ' +settings.colors.arrow+ ';      \
+                    border-width: 0 3px 3px 0;                      \
+                }                                                   \
+                .arrow--U span{                                     \
+                    transform: rotate(-135deg);                     \
+                }                                                   \
+                .arrow--R span{                                     \
+                    transform: rotate(-45deg);                      \
+                }                                                   \
+                .arrow--D span{                                     \
+                    transform: rotate(45deg);                       \
+                }                                                   \
+                .arrow--L span{                                     \
+                    transform: rotate(135deg);                      \
+                }                                                   \
                 ').appendTo('head');
         }
 
-        setDefaultClasses('titleColor', 'IconColor', 'arrowColor', '#fff', '#eee');
+        setDefaultClasses();
 
         /**
          * Get a string and return a string with '-' format
@@ -131,18 +142,23 @@
          * @param {String} state name of an existing State
          * @return {object} jQuery Object
          */
-        $.fn.StateTracker.createStage = function(name, state, index){
-            if(!name) return;
-            var idStg = (getSlashedNames(name)) ? 'id="' +getSlashedNames(name)+ '"' : "";
-            var stage = $('<div data-index="' +index+ '" '+idStg+ '></div>').addClass('stTracker__stage');
-            stage.prop('state', state)
+        $.fn.StateTracker.createStage = function(stage, state){
+            if(!stage) return;
+            var idStg = (getSlashedNames(stage.title)) ? 'id="' +getSlashedNames(stage.title)+ '"' : "";
+            var obStage = $('<div data-index="' +stage.index+ '" '+idStg+ '></div>').addClass('stTracker__stage');
+            obStage.prop('state', state)
             $('<div></div>').addClass('stTracker__icon')
                             .append('<i></i>')
-                            .appendTo(stage);
+                            .appendTo(obStage);
             $('<div></div>').addClass('stTracker__title')
-                            .append($('<p></p>').html(name))
-                            .appendTo(stage);
-            return stage;
+                            .append($('<p></p>').html(stage.title))
+                            .appendTo(obStage);
+            if(stage.subtitle){
+                $('<div></div>').addClass('stTracker__subtitle')
+                                .append($('<p></p>').html(stage.subtitle))
+                                .appendTo(obStage);
+            }
+            return obStage;
         }
 
         $.fn.StateTracker.createArrow = function(direction){
@@ -161,7 +177,8 @@
 
             /**
              * @typedef {Object} Stage
-             * @property {String} name name of Stage
+             * @property {String} title title of Stage
+             * @property {String} subtitle subtitle of Stage
              * @property {String} state name of init state of stage
              * @property {String} index index of stage
              */
@@ -170,8 +187,9 @@
              * @param {Stage} stage Stage object to add
              */
             self.addStage = function(stage){
-                self.stages[getSlashedNames(stage.name, true)] = {
-                    name: stage.name,
+                self.stages[getSlashedNames(stage.title, true)] = {
+                    title: stage.title,
+                    subtitle: stage.subtitle,
                     state: stage.state,
                     index: stage.index
                 };
@@ -244,13 +262,13 @@
                 $(cont).empty()
                 var keys = Object.keys(self.stages);
                 keys.sort(function(a, b){ return self.stages[a].index - self.stages[b].index; });
-                keys.forEach(function(name, index){
-                    var stage = self.stages[name];
+                keys.forEach(function(title, index){
+                    var stage = self.stages[title];
                     var state = self.states[getSlashedNames(stage.state)];
                     var minicont = $('<div></div>').addClass('stTracker__box');
                     if(index > 0)
                         $.fn.StateTracker.createArrow('R').appendTo(minicont);
-                    $.fn.StateTracker.createStage(stage.name, state, stage.index).appendTo(minicont);
+                    $.fn.StateTracker.createStage(stage, state).appendTo(minicont);
                     $(minicont).appendTo(cont);
                 });
             };
@@ -263,7 +281,7 @@
                 if(Array.isArray(stages)){
                     stages.forEach(function(stage, ind){
                         $(stage).children('.stTracker__icon')
-                                .children('i').addClass((stage.state && stage.state.icon)? stage.state.icon: "").css('color', (stage.state && stage.state.color)? stage.state.color: "");
+                                .children('i').addClass((stage.state && stage.state.icon)? stage.state.icon: "").css('color', (stage.state && stage.state.color)? stage.state.color: "inherit");
                     });
                 }
             }
